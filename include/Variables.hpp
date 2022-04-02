3b699e08b22e52aa4bb41345184f8bda6cef3a02
Josh Santos <nerdsville@fedoraproject.org>
2022-04-02 18:27:20 +0700

    Add default font

--- include/Variables.hpp
+++ include/Variables.hpp
@@ -1,4 +1,6 @@
 #pragma once
+
+#include <birb2d/Font.hpp>
 #include <birb2d/Values.hpp>
 
 using namespace Birb;
@@ -11,6 +13,7 @@ static ApplicationInfo appInfo;
 
 static std::string mononoki_bold = appInfo.ResLocation + "/fonts/mononoki-Bold.ttf";
 static std::string mononoki_regular = appInfo.ResLocation + "/fonts/mononoki-Regular.ttf";
+static Font DefaultFont(mononoki_regular);
 
 /* Variables */
 static int totalRam = 1024;
